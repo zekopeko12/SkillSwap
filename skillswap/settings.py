d6f1c62c86ade3c78ab1b95a61e46f43732b3448
zekopeko12 <antoniozeko3@gmail.com>
2026-06-15 13:50:39 +0200

    Settings changed

--- skillswap/settings.py
+++ skillswap/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8n%t_&d!%u4#9m@ltelf-=g)4p45*7_9jw2b6-zd8(2wxu^20q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'True') == "True"
+DEBUG = True
 
 ALLOWED_HOSTS = ['skillswap-k1ap.onrender.com', '127.0.0.1']
 
